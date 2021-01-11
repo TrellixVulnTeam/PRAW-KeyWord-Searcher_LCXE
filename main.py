2ceed62b8b3126b38e2910e46477f33521fef7f2
Hayden Rieder <haydenrieder50@hotmail.com>
2021-01-10 19:11:48 -0500

    Imported time var and created infinite loop
    - Finished method for checking if RTX is mentioned
    - Created infinite loop for bot
    - Made method active evry .5 seconds
    - Changed limit of posts checked to just most recent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import praw
+import time
+from requests.exceptions import HTTPError, ConnectionError, Timeout
 
 reddit = praw.Reddit(client_id='OECYB-E6JD01mg',
                      client_secret='BVZEzZ7_Y9B1qo7frGQ_IHvfha3vPA',
@@ -8,9 +10,22 @@ reddit = praw.Reddit(client_id='OECYB-E6JD01mg',
 
 my_gpu_sub = reddit.submission(url='https://www.reddit.com/user/HaydenR50/comments/ktap3c/'
                                    'gpu_availability_bot_thread/?utm_source=share&utm_medium=web2x&context=3')
-new_bpc = reddit.subreddit('buildapcsales').new(limit=10)
+new_bpc = reddit.subreddit('buildapcsales').new(limit=1)
 
-for submission in new_bpc:
-    if '[GPU]' in submission.title or '[gpu]' in submission.title:
-        reply_text = 'Here is a GPU Card Available: ' + submission.url
-        my_gpu_sub.reply(reply_text)
+timer = .5
+errors = 1
+while True:
+    try:
+        time.sleep(timer)
+        for submission in new_bpc:
+            if 'RTX' in submission.title or '[Restock]' in submission.title:
+                my_gpu_sub.reply('An RTX Card/Restock has been mentioned: ' + submission.url)
+        timer = .5
+        errors = 1
+    except(HTTPError, ConnectionError, Timeout) as e:
+        if errors == 1:
+            my_gpu_sub.reply('Problem Occurred! This has occurred only once.')
+        else:
+            my_gpu_sub.reply('Problem Occurred! This has occurred ' + str(errors) + ' times. Reddit may be down.')
+        timer = (errors * 300)
+        errors += 1
